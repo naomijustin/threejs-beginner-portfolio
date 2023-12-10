3760b123e2585b3705680cec631263877d8e6406
naomijustin <naomi.z.justin@gmail.com>
2023-12-11 08:24:59 +1100

    added renderer

--- main.js
+++ main.js
@@ -3,4 +3,8 @@ import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75, );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderList({
+    canvas: document.querySelector("#bg")
+});
