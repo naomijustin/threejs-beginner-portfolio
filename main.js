e8d720220d77a460e273a10a18e07e1eac755672
naomijustin <naomi.z.justin@gmail.com>
2024-01-18 21:21:11 +1100

    Added Renderer and moved camera

--- main.js
+++ main.js
@@ -8,3 +8,10 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 const renderer = new THREE.WebGLRenderList({
     canvas: document.querySelector("#bg"),
 });
+
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+
+camera.position.setZ(30);
+
+renderer.render( scene, camera );
