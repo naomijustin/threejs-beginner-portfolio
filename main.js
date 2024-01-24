101e043280a8c4486f5c45a8b2e00a76e54452fe
naomijustin <naomi.z.justin@gmail.com>
2024-01-24 20:10:59 +1100

    added point light

--- main.js
+++ main.js
@@ -14,16 +14,20 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
 
-camera.position.setZ(30);
-//camera.position.z = 5;
+camera.position.z = 30;
 
 renderer.render( scene, camera );
 
 const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
-const material = new THREE.MeshBasicMaterial( {color: 0xFF6347, wireframe: true} );
+const material = new THREE.MeshStandardMaterial( {color: 0xFF6347} );
 const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus);
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(7,7,7);
+
+scene.add(pointLight);
 
 function animate() {
     requestAnimationFrame(animate);
