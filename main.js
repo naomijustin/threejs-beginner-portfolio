051de680188b23146ae51ba01608efbee5b1fefc
naomijustin <naomi.z.justin@gmail.com>
2024-01-29 18:41:27 +1100

    added randomly generated stars

--- main.js
+++ main.js
@@ -40,13 +40,25 @@ scene.add(pointLight, ambientLight, lightHelper, gridHelper);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
+function addStar() {
+    const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+    const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+    const star = new THREE.Mesh(geometry, material);
+
+    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100 ));
+    star.position.set(x, y, z);
+    scene.add(star);
+}
+
+Array(200).fill().forEach(addStar);
 
 function animate() {
     requestAnimationFrame(animate);
-    renderer.render(scene, camera);
     torus.rotation.x += 0.01;
     torus.rotation.y += 0.005;
     torus.rotation.z += 0.01;
+    controls.update();
+    renderer.render(scene, camera);
 }
 
 animate();
